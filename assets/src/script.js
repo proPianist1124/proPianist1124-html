236f971d9b3fc2e22b0b29232bab64f96d79c222
proPianist1124 <phantomforces11242009@gmail.com>
2024-01-03 12:40:38 -0800

    ui rework and folder organization

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,6 +1,6 @@
 main()
 async function main(){
-    const projects_response = await fetch("src/data/projects.json")
+    const projects_response = await fetch("assets/src/data/projects.json")
     const projects_data = await projects_response.json()
 
     for(let i=0; i<projects_data.projects.length; i++){
@@ -8,11 +8,11 @@ async function main(){
         projects.innerHTML = `${projects.innerHTML}<a href = "${projects_data.projects[i].url}" target = "_blank"><div class = "card-hoverable">${projects_data.projects[i].name}</div></a>`
     }
 
-    const socials_response = await fetch("src/data/socials.json")
+    const socials_response = await fetch("assets/src/data/socials.json")
     const socials_data = await socials_response.json()
 
     for(let i=0; i<socials_data.socials.length; i++){
         const socials = document.getElementById("socials")
-        socials.innerHTML = `${socials.innerHTML}<a href = "${socials_data.socials[i].url}"><button class = "${socials_data.socials[i].class}"><i class = "${socials_data.socials[i].icon}"></i> ${socials_data.socials[i].name}</button></a><br>`
+        socials.innerHTML = `${socials.innerHTML}<a href = "${socials_data.socials[i].url}"><button class = "${socials_data.socials[i].class}"><i class = "${socials_data.socials[i].icon}"></i></button></a><br>`
     }
 }
